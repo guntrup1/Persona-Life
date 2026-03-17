4964b2c41fbd26ba2831451922939e70af244746
guntrup1 <98844595+guntrup1@users.noreply.github.com>
2026-03-17 23:32:38 +0100

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -1,7 +1,7 @@
 import express, { type Express } from "express";
 import fs from "fs";
 import path from "path";
-
+ 
 export function serveStatic(app: Express) {
   const distPath = path.resolve(__dirname, "public");
   if (!fs.existsSync(distPath)) {
@@ -9,11 +9,11 @@ export function serveStatic(app: Express) {
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
     );
   }
-
+ 
   app.use(express.static(distPath));
-
+ 
   // fall through to index.html if the file doesn't exist
-  app.use("/{*path}", (_req, res) => {
+  app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
