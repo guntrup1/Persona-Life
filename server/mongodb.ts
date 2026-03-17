ead38571c5dc0b1417c1572180782f356c9b2374
guntrup1 <98844595+guntrup1@users.noreply.github.com>
2026-03-17 10:19:24 +0100

    Update mongodb.ts

--- server/mongodb.ts
+++ server/mongodb.ts
@@ -20,6 +20,9 @@ export async function connectMongoDB() {
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password_hash: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verifyToken: { type: String, default: null },
+  verifyTokenExpires: { type: Date, default: null },
 }, { timestamps: true });
 
 const userDataSchema = new mongoose.Schema({
